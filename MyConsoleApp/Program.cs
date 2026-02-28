3106c32829000d262721dab28a2bf5530d94d69a
AndersonTheoo <andersontheoo@gmail.com>
2026-02-27 23:38:36 -0300

    Ampliando conhecimento básicos em C#

--- MyConsoleApp/Program.cs
+++ MyConsoleApp/Program.cs
@@ -1,3 +1,59 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Exibindo mensagens simples no console
 Console.WriteLine("Hello, World!");
-Console.WhiteLine("The current time is " Datetime.Now);
+Console.WriteLine("The current time is " + DateTime.Now);
+
+// Exibindo múltiplas mensagens e controlando espaços manualmente
+Console.WriteLine("Congratulations!");
+Console.Write(" ");
+Console.Write("You wrote your first line of code in C#!");
+
+// Diferença entre WriteLine (quebra linha) e Write (não quebra linha)
+Console.WriteLine("This is the first line.");
+Console.Write("This is the second line.");
+
+// Declaração, atribuição e exibição de variável string
+string firstName;
+firstName = "Bob";
+Console.WriteLine(firstName);
+
+//Reatribuir o valor de uma variável
+string firstName;
+firstName = "Anderson";
+Console.WriteLine(firstName);
+firstName = "Alex";
+Console.WriteLine(firstName);
+firstName = "Eduardo";
+Console.WriteLine(firstName);
+firstName = "ericles";
+Console.WriteLine(firstName);
+
+//Declaração e atribuição de uma variável em uma única linha
+string firstName = "Anderson";
+Console.WriteLine(firstName);
+
+
+//variável local tipada implicitamente.
+Você não escreveu o tipo, mas o C# descobre automaticamente.
+var message = "Hello world!";
+Console.WriteLine(message);
+
+
+//Hello, Bob! You have 3 messages in your inbox. The temperature is 34.4 celsius.
+var firstName = "Anderson";
+var message = 3;
+var temp = 34.4;
+Console.WriteLine("Hello, " + firstName + "!" + " You have " + message + " messages in your inbox." + " The temperature is " + temp + " Celsius.");
+
+//Hello, Bob! You have 3 messages in your inbox. The temperature is 34.4 celsius.
+string name = "Bob";
+int messages = 3;
+decimal temperature = 34.4m;
+
+Console.Write("Hello, ");
+Console.Write(name);
+Console.Write("! You have ");
+Console.Write(messages);
+Console.Write(" messages in your inbox. The temperature is ");
+Console.Write(temperature);
+Console.Write(" celsius.");
+
