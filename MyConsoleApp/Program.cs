7d9e94ab33a0f6ed65aafe14d9b73ed0020d8a96
AndersonTheoo <andersontheoo@gmail.com>
2026-03-03 10:50:52 -0300

    Adicionando exercícios sobre interpolação e decimal

--- MyConsoleApp/Program.cs
+++ MyConsoleApp/Program.cs
@@ -1,4 +1,5 @@
-﻿// Exibindo mensagens simples no console
+﻿
+// Exibindo mensagens simples no console
 Console.WriteLine("Hello, World!");
 Console.WriteLine("The current time is " + DateTime.Now);
 
@@ -57,3 +58,86 @@ Console.Write(" messages in your inbox. The temperature is ");
 Console.Write(temperature);
 Console.Write(" celsius.");
 
+
+//Sequencia de escape 
+Console.WriteLine("Hello\nWorld!"); //exibir quebra de linha
+Console.WriteLine("Hello\tWorld!"); //exibir tabulação
+Console.WriteLine("Hello \"World\"!"); //exibir aspas duplas
+Console.WriteLine("C: \\sources\\repos\\MyConsoleApp"); //exibir barra invertida
+
+
+//Gerando faturas para o cliente "Contoso Corp" ...
+Console.WriteLine("Generating invoices for custumer \"Contoso Corp\" ... \n");
+Console.WriteLine("Invoice: 1021\t\tComplete!");
+Console.WriteLine("Invoice: 1022\t\tComplete!");
+Console.Write("\nOutput Directory:\t");
+
+//Exibindo o caminho do diretório onde os arquivos de fatura foram salvos
+Console.WriteLine(@"    c:\source\repos
+         (This is where you code goes)");
+
+//Exibindo o caminho do diretório onde os arquivos de fatura foram 
+//salvos usando verbatim string literal
+Console.Write(@"c:\invoices");
+
+
+//Para gerar faturas em japonês:
+Console.WriteLine("Generating invoices for customer \"Contoso Corp\" ... \n");
+Console.WriteLine("Invoice: 1021\t\tComplete!");
+Console.WriteLine("Invoice: 1022\t\tComplete!");
+Console.Write("\nOutput Directory:\t");
+Console.Write(@"c:\invoices");
+
+// To generate Japanese invoices:
+// Nihon no seikyū-sho o seisei suru ni wa:
+Console.Write("\n\n\u65e5\u672c\u306e\u8acb\u6c42\u66f8\u3092\u751f\u6210\u3059\u308b\u306b\u306f\uff1a\n\t");
+// User command to run an application
+Console.WriteLine(@"c:\invoices\app.exe -j");
+
+
+// Concatenando strings usando o operador +
+string firstName = "Anderson";
+string greeting = "Hello";
+string message = greeting + " " + firstName + "!";
+Console.WriteLine(message);
+
+// Concatenando strings usando interpolação de string
+string firstName = "Anderson";
+string greeting = "Hello";
+string message = $"{greeting} {firstName}!";
+Console.WriteLine(message);
+
+string firstName = "Anderson";
+string secondName = "Teotonio da Silva";
+String message = $"Hello {firstName} {secondName}!";
+Console.WriteLine(message);
+
+
+// Concatenando strings usando interpolação de stringx
+int version = 11;
+string updateText = "Update to Windows";
+string message = $"{updateText} {version}";
+Console.WriteLine(message);
+
+int version = 11;
+string updateText = "Update to Windows";
+Console.WriteLine($"{updateText} {version}!");
+
+
+//Interpolação de string com expressões
+//@ de prefixo literal textual e o símbolo $ de interpolação de cadeia de caracteres juntos.
+string projectName = "First-Project";
+Console.WriteLine($@"C:\Output\{projectName}\Data");
+
+//atividade de interpolação de string com expressões
+string projectName = "ACME";
+string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
+
+//resultado
+string projectName = "ACME";
+string englishLocation = $@"c:\Exercise\{projectName}\data.txt";
+Console.WriteLine($"View English output:\n\t{englishLocation}\n");
+
+string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
+string russianLocation = $@"c:\Exercise\{projectName}\ru-RU\data.txt";
+Console.WriteLine($"{russianMessage}:\n\t{russianLocation}\n");
---
+++ MyConsoleApp/Program2.cs
@@ -0,0 +1,29 @@
+﻿/*
+// Operadores Aritméticos
+int num1 = 12;
+int num2 = 5;
+Console.WriteLine(num1 + num2); //soma
+Console.WriteLine(num1 - num2); //subtração
+Console.WriteLine(num1 * num2); //multiplicação
+Console.WriteLine(num1 / num2); //divisão
+Console.WriteLine(num1 % num2); //resto da divisão
+
+// Operadores de String
+string firstName = "Anderson";
+int itensVendidos = 9;
+Console.WriteLine(firstName + " vendeu " + itensVendidos + " itens.");
+
+
+//Operadores de Atribuição
+string firstName = "Anderson";
+int itensVendidos = 9;
+Console.WriteLine(firstName + " Vendeu " + (itensVendidos + 7) + " itens ");
+
+decimal decimalQuocient = 7.0m / 5;
+Console.WriteLine($"Decimal quocient: {decimalQuocient}");
+*/
+
+int first = 7;
+int second = 5;
+decimal quocient = (decimal)first / (decimal)second;
+Console.WriteLine(quocient);
