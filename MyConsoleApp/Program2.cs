6f427f0e93e22cf7a15128191134d535af4d27ff
AndersonTheoo <andersontheoo@gmail.com>
2026-03-03 12:37:47 -0300

    Modulo 2

--- MyConsoleApp/Program2.cs
+++ MyConsoleApp/Program2.cs
@@ -1,29 +1,89 @@
 ﻿/*
 // Operadores Aritméticos
-int num1 = 12;
-int num2 = 5;
-Console.WriteLine(num1 + num2); //soma
-Console.WriteLine(num1 - num2); //subtração
-Console.WriteLine(num1 * num2); //multiplicação
-Console.WriteLine(num1 / num2); //divisão
-Console.WriteLine(num1 % num2); //resto da divisão
+    int num1 = 12;
+    int num2 = 5;
+    Console.WriteLine(num1 + num2); //soma
+    Console.WriteLine(num1 - num2); //subtração
+    Console.WriteLine(num1 * num2); //multiplicação
+    Console.WriteLine(num1 / num2); //divisão
+    Console.WriteLine(num1 % num2); //resto da divisão
 
 // Operadores de String
-string firstName = "Anderson";
-int itensVendidos = 9;
-Console.WriteLine(firstName + " vendeu " + itensVendidos + " itens.");
+    string firstName = "Anderson";
+    int itensVendidos = 9;
+    Console.WriteLine(firstName + " vendeu " + itensVendidos + " itens.");
 
 
 //Operadores de Atribuição
-string firstName = "Anderson";
-int itensVendidos = 9;
-Console.WriteLine(firstName + " Vendeu " + (itensVendidos + 7) + " itens ");
+    string firstName = "Anderson";
+    int itensVendidos = 9;
+    Console.WriteLine(firstName + " Vendeu " + (itensVendidos + 7) + " itens ");
 
-decimal decimalQuocient = 7.0m / 5;
-Console.WriteLine($"Decimal quocient: {decimalQuocient}");
+    decimal decimalQuocient = 7.0m / 5;
+    Console.WriteLine($"Decimal quocient: {decimalQuocient}");
+
+
+    int first = 7;
+    int second = 5;
+    decimal quocient = (decimal)first / (decimal)second;
+    Console.WriteLine(quocient);
+
+
+    double numero = 9.7;
+    int inteiro = (int)numero;
+    Console.WriteLine(inteiro);
+
+//Operadores de Resto- aprende se um número é divisível por outro
+    Console.WriteLine($"Modulus of 200 / 5 : {200 % 5}");
+    Console.WriteLine($"Modulos of 7 / 5 : {7 % 5}");
+
+//Precedência de operadores
+    int value1 = 3 + 4 * 5;
+    int value2 = (3 + 4) * 5;
+    Console.WriteLine(value1);
+    Console.WriteLine(value2);
+
+//Operadores de Incremento e Decremento
+    int x = 10;
+    x += 5;
+    Console.WriteLine(x);
+    //funciona com strings também
+    string nome = "Theo";
+    nome += " Silva";
+
+    int value = 1;
+
+    value = value + 1;
+    Console.WriteLine("First increment: " + value);
+
+    value += 1;
+    Console.WriteLine("Second increment: " + value);
+
+    value++;
+    Console.WriteLine("Third increment: " + value);
+
+    value = value - 1;
+    Console.WriteLine("First decrement: " + value);
+
+    value -= 1;
+    Console.WriteLine("Second decrement: " + value);
+
+    value--;
+    Console.WriteLine("Third decrement: " + value);
+
+
+int value = 1;
+value++;
+Console.WriteLine("First increment: " + value);
+
+//ATIVIDADE
+        double fahrenheit = 94;
+        double Celsius = (fahrenheit - 32) * 5 / 9;
+        Console.WriteLine ($"The temperature is: {Celsius:F1} Celsius.");
+
+        int fahrenheit = 94;
+        decimal celsius = (fahrenheit - 32m) * (5m / 9m);
+        Console.WriteLine("The temperature is " + celsius + " Celsius.");
+
 */
 
-int first = 7;
-int second = 5;
-decimal quocient = (decimal)first / (decimal)second;
-Console.WriteLine(quocient);
