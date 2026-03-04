102fbad31a3dde09eda19c2dfb0887684e9250fd
AndersonTheoo <andersontheoo@gmail.com>
2026-03-04 11:19:35 -0300

    Novas atividades

---
+++ MyConsoleApp/Program3.cs
@@ -0,0 +1,42 @@
+﻿/*
+-Aprenda a desenvolver aplicativos básicos dividindo o trabalho em partes menores.
+-dentifique os tipos de dados apropriados para armazenar e trabalhar com variáveis.
+-Execute operações matemáticas para determinar os resultados.
+-Aplique técnicas de formatação para exibir a saída.*/
+
+int currentAssignment = 5;
+
+int sophia1 = 93;
+int sophia2 = 87;
+int sophia3 = 98;
+int sophia4 = 95;
+int sophia5 = 100;
+
+int nicolas1 = 80;
+int nicolas2 = 83;
+int nicolas3 = 82;
+int nicolas4 = 88;
+int nicolas5 = 85;
+
+int zahirah1 = 84;
+int zahirah2 = 96;
+int zahirah3 = 73;
+int zahirah4 = 85;
+int zahirah5 = 79;
+
+int jeong1 = 90;
+int jeong2 = 92;
+int jeong3 = 98;
+int jeong4 = 100;
+int jeong5 = 97;
+
+int mediaSophia = (sophia1 + sophia2 + sophia3 + sophia4 + sophia5) / currentAssignment;
+int mediaNicolas = (nicolas1 + nicolas2 + nicolas3 + nicolas4 + nicolas5) / currentAssignment;
+int mediaZahirah = (zahirah1 + zahirah2 + zahirah3 + zahirah4 + zahirah5) / currentAssignment;
+int mediaJeong = (jeong1 + jeong2 + jeong3 + jeong4 + jeong5) / currentAssignment;
+
+Console.WriteLine($"Média de Sophia: {mediaSophia}" + "\tA");
+Console.WriteLine($"média de Nicolas: {mediaNicolas}" + "\tB");
+Console.WriteLine($"media de Zahirah: {mediaZahirah}" + "\tB");
+Console.WriteLine($"média de Jeong: {mediaJeong}" + "\tA");
+
